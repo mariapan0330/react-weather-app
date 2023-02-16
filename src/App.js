82a3514c0d51b58e4bd34c274a48b6cfc822c046
mariapan0330 <maria.pan0330@gmail.com>
2023-02-16 03:12:09 -0600

    changed api key functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Landing from "./components/Landing.jsx"
 import './App.css'
-import { apiKey } from './key.js'
+// import { apiKey } from './key.js'
 
 async function getWeatherData(city, country, zip, lat, lon){
     console.log('Getting Weather Data...');
@@ -14,16 +14,16 @@ async function getWeatherData(city, country, zip, lat, lon){
     let res;
     // latitude and longitude at the top bc it's the most specific.
     if (lat) {
-        res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
+        res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`)
 
     } else if (city && !country){
-        res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`)
+        res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`)
 
     } else if (city && country){
-        res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=imperial`)
+        res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`)
         
     } else if (zip){
-        res = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zip},${country}&appid=${apiKey}&units=imperial`)
+        res = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zip},${country}&appid=${API_KEY}&units=imperial`)
     }
 
     let data = await res.json()
@@ -31,7 +31,7 @@ async function getWeatherData(city, country, zip, lat, lon){
     if (zip){
         let zipCity = data['name']
         let zipCountry = data['country']
-        res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${zipCity},${zipCountry}&appid=${apiKey}&units=imperial`)
+        res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${zipCity},${zipCountry}&appid=${API_KEY}&units=imperial`)
         data = await res.json()
     }
     
